83ec71ccc6a97ddbd3bda352050be77e6154649b
Melvin Carvalho <melvincarvalho@gmail.com>
2021-11-21 18:48:05 +0100

    add Footer component

---
+++ Footer.js
@@ -0,0 +1,10 @@
+import { html } from 'https://unpkg.com/spux?module'
+
+export default function Footer (props) {
+  return html`
+    <hr />
+    <footer class="row">
+      ${text}
+    </footer>
+  `
+}
