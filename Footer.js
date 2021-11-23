0429e28d265a54269dfa46bbd1da237bb2efa1ca
Melvin Carvalho <melvincarvalho@gmail.com>
2021-11-23 11:09:35 +0100

    add genesis hyperlink

--- Footer.js
+++ Footer.js
@@ -3,7 +3,7 @@ import { html } from 'https://unpkg.com/spux?module'
 export default function Footer(props) {
   return html`
     <footer>
-      <i>${props.text} ${props.genesis ? ' | genesis: ' + props.genesis : ''}</i>
-    </footer>
-  `
+      ${props.text} ${props.genesis ? ` | Genesis | 
+      <i><a href="https://gitmark.info/${props.genesis}">${props.genesis}</a></i> | <a href="https://git-mark.com/">Gitmark</a>` : ''}
+    </footer>`
 }
