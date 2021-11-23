cb7983296f68f11dd103a649b5bead2e42a6a4e1
Melvin Carvalho <melvincarvalho@gmail.com>
2021-11-23 11:17:27 +0100

    split footer on genesis

--- Footer.js
+++ Footer.js
@@ -1,9 +1,16 @@
 import { html } from 'https://unpkg.com/spux?module'
 
 export default function Footer(props) {
-  return html`
+  if (props.genesis) {
+    return html`
     <footer>
-      ${props.text} ${props.genesis ? ` | Genesis | 
-      <i><a href="https://gitmark.info/${props.genesis}">${props.genesis}</a></i> | <a href="https://git-mark.com/">Gitmark</a>` : ''}
+      ${props.text} | Genesis | <i><a style="color:blue" target="_blank" href="https://gitmark.info/${props.genesis}">${props.genesis}</a></i> | <a style="color:blue" target="_blank" href="https://git-mark.com/">Gitmark</a>
     </footer>`
+  } else {
+    return html`
+    <footer>
+      ${props.text}
+    </footer>
+    `
+  }
 }
